52cd35c28599e97b13a2c01979ff6c830754aea1
SnooZerken <roel.arys@gmail.com>
2020-08-11 10:35:32 +0200

    Changed url of api

--- src/http-commons.js
+++ src/http-commons.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:8080",
+  //baseURL: "http://localhost:8080",
+  baseURL: "http://helpcenter-eprochelpcenter.apps.us-east-1.starter.openshift-online.com:80",
   headers: {
     "Content-type": "application/json",
   },

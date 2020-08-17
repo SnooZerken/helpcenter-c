78c2550daf5ab33791a6911ca051f9c2b095241d
SnooZerken <roel.arys@gmail.com>
2020-08-17 14:11:24 +0200

    changed main menu

--- src/App.vue
+++ src/App.vue
@@ -2,9 +2,10 @@
   <v-app>
 
     <v-app-bar app dense dark>
-        <v-btn small class="ml-2" to="/"><v-icon small>mdi-information</v-icon>home</v-btn>
-        <v-btn small class="ml-2" to="/sections"><v-icon small>mdi-format-list-bulleted</v-icon>sections</v-btn>
-        <v-btn small class="ml-2" to="/about"><v-icon small>mdi-information</v-icon>about</v-btn>
+        <v-btn small class="ml-2" to="/">home</v-btn>
+        <v-btn small class="ml-2" to="/sections" v-if="$keycloak.authenticated">sections</v-btn>
+        <v-btn small class="ml-2" to="/about">about</v-btn>
+        <v-btn small class="ml-2" @click="$keycloak.loginFn" v-if="!$keycloak.authenticated">Log in</v-btn>
         <v-btn small class="ml-2" @click="$keycloak.logoutFn" v-if="$keycloak.authenticated">Log out</v-btn>
     </v-app-bar>
 
